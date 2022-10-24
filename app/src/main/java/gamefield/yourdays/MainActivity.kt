4c1c19e72c19d9750c7a291907441402c2c05826
Ivan Balitsky <balitskyivan@gmail.com>
2022-10-24 23:49:39 +0300

    add month card

--- app/src/main/java/gamefield/yourdays/MainActivity.kt
+++ app/src/main/java/gamefield/yourdays/MainActivity.kt
@@ -1,5 +1,6 @@
 package gamefield.yourdays
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.SeekBar
@@ -18,6 +19,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        startActivity(Intent(this, TestActivity::class.java))
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
