e6d00b121196cdf333e741d7aecfb6e4b478c092
Ivan Balitsky <balitskyivan@gmail.com>
2023-02-01 13:04:47 +0300

    Fix resolution

--- app/src/main/java/gamefield/yourdays/domain/usecase/time_logic/IsNeedToAddDaysInMonthUseCase.kt
+++ app/src/main/java/gamefield/yourdays/domain/usecase/time_logic/IsNeedToAddDaysInMonthUseCase.kt
@@ -8,10 +8,11 @@ class IsNeedToAddDaysInMonthUseCase {
     private val calendar = Calendar.getInstance()
 
     operator fun invoke(month: Month): Boolean {
+        calendar.toInstant()
         val week = calendar.get(Calendar.WEEK_OF_MONTH)
         val currentDayInWeek = calendar.get(Calendar.DAY_OF_WEEK)
 
-        if (month.weeks.size - 1 < week || month.weeks[week].days[currentDayInWeek - 1].emotion == null)
+        if (month.weeks.size < week || month.weeks[week - 1].days[currentDayInWeek - 1].emotion == null)
             return true
 
         return false
